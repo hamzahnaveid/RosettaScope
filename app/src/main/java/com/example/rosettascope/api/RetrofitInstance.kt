de3e330044512496de089591f64bf5f9dde950b0
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2025-11-06 08:37:23 +0000

    Set timeout for Retrofit requests

--- app/src/main/java/com/example/rosettascope/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/rosettascope/api/RetrofitInstance.kt
@@ -1,14 +1,23 @@
 package com.example.rosettascope.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     //accessing API through ngrok tunnel
     private const val BASE_URL = "https://subopaquely-unirradiative-bradley.ngrok-free.dev"
 
+    val okHttpClient = OkHttpClient.Builder()
+        .readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .build()
+
     val api: TranslationApiService by lazy {
         Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
