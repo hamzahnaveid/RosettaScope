f09ce44578420c4558bb59bf343da677f980cbd3
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-03-03 03:50:51 +0000

    Signup screen

--- app/src/main/java/com/example/rosettascope/screens/SignupScreen.kt
+++ app/src/main/java/com/example/rosettascope/screens/SignupScreen.kt
@@ -5,20 +5,26 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,9 +32,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -62,12 +72,37 @@ fun SignupScreen(modifier: Modifier = Modifier, navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Sign Up",
-            fontSize = 32.sp
+        Image(painter = painterResource(id = R.drawable.transparent_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 54.dp, start = 16.dp)
+                .height(100.dp)
+                .align(Alignment.Start)
+                .offset(x = (-20).dp)
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Sign Up",
+            style = TextStyle(
+                fontSize = 28.sp,
+                fontWeight = FontWeight(500),
+                color = Color.White
+            ),
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .align(Alignment.Start)
+        )
+
+        Text("Start your journey in language learning with Rosetta Scope",
+            style = TextStyle(
+                fontSize = 20.sp,
+                color = Color.White
+            ),
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
 
         OutlinedTextField(
             value = email,
@@ -75,8 +110,15 @@ fun SignupScreen(modifier: Modifier = Modifier, navController: NavController) {
                 email = it
             },
             label = {
-                Text(text = "Email")
-            }
+                Text(text = "Email Address")
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = colorResource(R.color.rosetta_yellow),
+                focusedLabelColor = colorResource(R.color.rosetta_yellow),
+                focusedTextColor = Color.White,
+                unfocusedContainerColor = Color.Transparent
+            )
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -88,16 +130,21 @@ fun SignupScreen(modifier: Modifier = Modifier, navController: NavController) {
             },
             label = {
                 Text(text = "Password")
-            }
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = colorResource(R.color.rosetta_yellow),
+                focusedLabelColor = colorResource(R.color.rosetta_yellow),
+                focusedTextColor = Color.White,
+                unfocusedContainerColor = Color.Transparent
+            )
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
         DisplayLangSpinner()
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(32.dp))
 
         Button(onClick = {
             if (email.isEmpty() || password.isEmpty()) {
@@ -166,16 +213,47 @@ fun SignupScreen(modifier: Modifier = Modifier, navController: NavController) {
                     })
                 queue.add(signUpRequest)
             }
-        }) {
-            Text(text = "Create account")
+        },
+            shape = MaterialTheme.shapes.large,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.rosetta_yellow)
+            ),
+            modifier = Modifier
+                .height(50.dp))
+        {
+            Text(text = "Sign Up",
+                style = TextStyle(
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight(500),
+                    color = Color.White
+                )
+            )
         }
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        TextButton(onClick = {
-            navController.navigate("login")
-        }) {
-            Text(text = "Already have an account? Login")
+        Row(
+            modifier = Modifier.padding(top=12.dp, bottom = 80.dp)
+        ) {
+            Text(
+                "Already have an account? ",
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+            )
+
+            Text(
+                "Login",
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(800),
+                    color = Color.White
+                ),
+                modifier = Modifier.clickable {
+                    navController.navigate("login")
+                }
+            )
         }
     }
 }
@@ -214,7 +292,15 @@ fun DisplayLangSpinner() {
             modifier = Modifier.menuAnchor(),
             trailingIcon = {
                 ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedState)
-            })
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = colorResource(R.color.rosetta_yellow),
+                focusedLabelColor = colorResource(R.color.rosetta_yellow),
+                focusedTextColor = Color.White,
+                unfocusedContainerColor = Color.Transparent
+            )
+        )
 
         ExposedDropdownMenu(
             expanded = expandedState,
