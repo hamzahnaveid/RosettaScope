1f2ea011469aeaaaa73c261b4be4b4927bb26f1b
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-03-27 08:49:44 +0000

    Pain Points activities setup

--- app/src/main/java/com/example/rosettascope/PPDrillsActivity.kt
+++ app/src/main/java/com/example/rosettascope/PPDrillsActivity.kt
@@ -1,27 +1,20 @@
 package com.example.rosettascope
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PainPointsActivity : AppCompatActivity() {
+class PPDrillsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pain_points)
+        setContentView(R.layout.activity_ppdrills)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun toDrills(v: View) {
-        val intent = Intent(this, PPDrillsActivity::class.java)
-        startActivity(intent)
     }
 }
